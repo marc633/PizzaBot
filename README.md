94849ad874e7f1198b13d9663430188e13cd8fec
Marc Episcope <marc633@gmail.com>
2020-09-28 10:27:23 -0500

    rewrote all of the code to use commands instead of events

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PizzaBot
 P-P-P-Pizza!
+
+invite link:
+https://discord.com/api/oauth2/authorize?client_id=754851728981229649&permissions=67648&scope=bot
+
--- pizzabot.py
+++ pizzabot.py
@@ -1,18 +1,35 @@
-# invite link:
-# https://discord.com/api/oauth2/authorize?client_id=754851728981229649&permissions=67648&scope=bot
-
-import discord
 import random
 import re
+import asyncio
+import discord
+from discord.ext import commands
+import youtube_dl
 
+client = commands.Bot(command_prefix = '!')
 token = open("token.txt", "r").read() # concealing token
-
-client = discord.Client()
 
 male = 'first_name_male.txt' # male name file
 female = 'first_name_female.txt' # female name file
 last = 'last_name.txt' # last name name file
-pizzatoppings = 'pizzatoppings.txt' # pizza toppings file
+
+def make_pizza(ingredients):
+    pizzatoppings = 'pizzatoppings.txt' # pizza toppings file
+
+    topping_list = random_topping(pizzatoppings, ingredients) # creating list for toppings
+
+    your_pizza = "" # creating string for output
+    your_pizza = ", ".join(topping_list[0:-1:]) # forming a string with everything but last ingredient because grammar
+        
+    if ingredients == 1: # 1 ingredient
+        return f"You should order a pizza with {topping_list[0]}."
+
+    elif ingredients == 2: # 2 ingredients
+        your_pizza = your_pizza + " and " + topping_list[-1] # adding back last ingredient
+        return f"You should order a pizza with {your_pizza}."
+
+    else:  # for ingredients between 3 and 7
+        your_pizza = your_pizza + ", and " + topping_list[-1] # adding back last ingredient
+        return f"You should order a pizza with {your_pizza}."
 
 def random_name(fname):
     lines = open(fname).read().splitlines()
@@ -24,55 +41,52 @@ def random_topping(fname, ingredients):
 
 @client.event # event decorator/wrapper
 async def on_ready():
-    print(f"We have logged in as {client.user}")
+    activity = discord.Game(name="P-P-P-PIZZA!", type=3)
+    await client.change_presence(status=discord.Status.online, activity=activity)
+    print(f"DING! Your fresh {client.user} is ready.")
 
 @client.event
 async def on_message(message):
-    print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}") # chat log for testing
+    # print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}") # chat log for testing
 
     if re.findall(r"(?i)\bpizza\b", message.content.lower()):  # adds reaction whenever "pizza" is mentioned
         await message.add_reaction("\U0001F355")
+    await client.process_commands(message)
 
-    elif "!firstmale" in message.content.lower():
-        await message.channel.send(f"```{random_name(male)}```")
+@client.command()
+async def firstmale(ctx):
+    await ctx.send(f"```{random_name(male)}```")
 
-    elif "!firstfemale" in message.content.lower():
-        await message.channel.send(f"```{random_name(female)}```")
+@client.command()
+async def firstfemale(ctx):
+    await ctx.send(f"```{random_name(female)}```")
 
-    elif "!fullmale" in message.content.lower():
-        await message.channel.send(f"```{random_name(male)} {random_name(last)}```")
+@client.command()
+async def fullmale(ctx):
+    await ctx.send(f"```{random_name(male)} {random_name(last)}```")
 
-    elif "!fullfemale" in message.content.lower():
-        await message.channel.send(f"```{random_name(female)} {random_name(last)}```")
+@client.command()
+async def fullfemale(ctx):
+    await ctx.send(f"```{random_name(female)} {random_name(last)}```")
 
-    elif "!football" in message.content.lower(): # returns link for scores (web scrape eventually)
-        await message.channel.send("https://sportsdata.usatoday.com/football/nfl/scores")
-    
-    elif re.match(r"(?i)^[!]?toppings\s?[1-7]?$", message.content.lower()):
+@client.command()
+async def football(ctx):
+    await ctx.send("https://sportsdata.usatoday.com/football/nfl/scores")
 
-        if re.fullmatch(r"(?i)^[!]?toppings$", message.content.lower()):
-            ingredients = random.randint(1, 3) # if no modifier is given, a random number between 1 and 3 is chosen for ingredients
+@client.command()
+async def toppings(ctx, ingredients=""):    
+    try:
+        if ingredients == "": ingredients = random.randint(1,3)
+        ingredients = int(ingredients)
 
-        else:
-            ingredients = int(re.findall(r"\b[1-7]\b", message.content.lower())[0]) # converting !toppings # into just a #
-    
-        topping_list = random_topping(pizzatoppings, ingredients) # creating list for toppings
+        if ingredients > 7 or ingredients < 1:
+            raise Exception
+        await ctx.send(make_pizza(ingredients))
 
-        your_pizza = "" # creating string for output
-        your_pizza = ", ".join(topping_list[0:-1:]) # forming a string with everything but last ingredient because grammar
-        
-        if ingredients == 1: # 1 ingredient
-            await message.channel.send(f"You should order a pizza with {topping_list[0]}.")
+    except ValueError:
+        await ctx.send("You must use only numbers.")
 
-        elif ingredients == 2: # 2 ingredients
-            your_pizza = your_pizza + " and " + topping_list[-1] # adding back last ingredient
-            await message.channel.send(f"You should order a pizza with {your_pizza}.")
-
-        else:  # for ingredients between 3 and 7
-            your_pizza = your_pizza + ", and " + topping_list[-1] # adding back last ingredient
-            await message.channel.send(f"You should order a pizza with {your_pizza}.")
-
-    elif "!toppings" in message.content.lower(): # catches all user user input for !toppings
-        await message.channel.send("You need to only use a single number between 1 and 7.")
+    except Exception:
+        await ctx.send("You must only use a single number between 1 and 7.")
 
 client.run(token)
